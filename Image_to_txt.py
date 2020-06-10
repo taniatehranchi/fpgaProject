91f0d309e0e1cafdefa1181e5d2f8d2f10105ae3
taravat <taravatpartt@gmail.com>
2020-06-10 14:24:17 +0430

    "python code.img block block shode va dar file text zakhire shod."

--- Image_to_txt.py
+++ Image_to_txt.py
@@ -3,4 +3,24 @@ import numpy as np
 
 I = cv2.imread('input.png')
 G = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
-np.savetxt('inputImage.txt', G, fmt='%d')
+
+f = open("inputImage.txt", "a")
+
+rowSlice=int(G.shape[0]/8)
+colSlice=int(G.shape[1]/8)
+row_pointer=0
+col_pointer=0
+
+
+for i in range(rowSlice):
+    for j in range(colSlice):
+        row_pointer=i*8
+        col_pointer=j*8
+        for index1 in range(8):
+            for index2 in range(8):
+                f.write(str(G[row_pointer][col_pointer])+'\n')
+                col_pointer=col_pointer+1
+            row_pointer = row_pointer + 1
+            col_pointer = j * 8
+
+
---
+++ imageCompression/file_reader.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    00:00:13 06/10/2020 
+-- Design Name: 
+-- Module Name:    file_reader - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity file_reader is
+end file_reader;
+
+architecture Behavioral of file_reader is
+
+begin
+
+
+end Behavioral;
+
