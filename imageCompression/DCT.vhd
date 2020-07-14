b85c24e202b94bce76068cf828b5127f4644161e
taravat <taravatpartt@gmail.com>
2020-07-14 23:21:55 +0430

    "quantization added + cleaned up a little"

--- imageCompression/DCT.vhd
+++ imageCompression/DCT.vhd
@@ -22,7 +22,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
-use std.textio.all;
 use work.matrix.all;
 
 entity DCT is
---
+++ imageCompression/isim/quantization_test_isim_beh.exe.sim/work/a_3923040884_2372691052.c
@@ -0,0 +1,342 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+/* This file is designed for use with ISim build 0x7708f090 */
+
+#define XSI_HIDE_SYMBOL_SPEC true
+#include "xsi.h"
+#include <memory.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+static const char *ng0 = "C:/Users/taravat/Desktop/fpga-project/fpgaProject/imageCompression/quantization_test.vhd";
+extern char *IEEE_P_3499444699;
+
+char *ieee_p_3499444699_sub_2213602152_3536714472(char *, char *, int , int );
+
+
+static void work_a_3923040884_2372691052_p_0(char *t0)
+{
+    char *t1;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+
+LAB0:    xsi_set_current_line(111, ng0);
+
+LAB3:    t1 = (t0 + 12599);
+    t3 = (t0 + 7120);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t5 + 56U);
+    t7 = *((char **)t6);
+    memcpy(t7, t1, 3U);
+    xsi_driver_first_trans_fast(t3);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+}
+
+static void work_a_3923040884_2372691052_p_1(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(112, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 150, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7184);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_2(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(113, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 180, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7248);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_3(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(114, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 210, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7312);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_4(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(115, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 240, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7376);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_5(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(116, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 270, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7440);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_6(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(117, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 300, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7504);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_7(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(118, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 50, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7568);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+static void work_a_3923040884_2372691052_p_8(char *t0)
+{
+    char t1[16];
+    char *t2;
+    unsigned char t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+
+LAB0:    xsi_set_current_line(119, ng0);
+
+LAB3:    t2 = ieee_p_3499444699_sub_2213602152_3536714472(IEEE_P_3499444699, t1, 10, 32);
+    t3 = (32U != 32U);
+    if (t3 == 1)
+        goto LAB5;
+
+LAB6:    t4 = (t0 + 7632);
+    t5 = (t4 + 56U);
+    t6 = *((char **)t5);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    memcpy(t8, t2, 32U);
+    xsi_driver_first_trans_fast(t4);
+
+LAB2:
+LAB1:    return;
+LAB4:    goto LAB2;
+
+LAB5:    xsi_size_not_matching(32U, 32U, 0);
+    goto LAB6;
+
+}
+
+
+extern void work_a_3923040884_2372691052_init()
+{
+	static char *pe[] = {(void *)work_a_3923040884_2372691052_p_0,(void *)work_a_3923040884_2372691052_p_1,(void *)work_a_3923040884_2372691052_p_2,(void *)work_a_3923040884_2372691052_p_3,(void *)work_a_3923040884_2372691052_p_4,(void *)work_a_3923040884_2372691052_p_5,(void *)work_a_3923040884_2372691052_p_6,(void *)work_a_3923040884_2372691052_p_7,(void *)work_a_3923040884_2372691052_p_8};
+	xsi_register_didat("work_a_3923040884_2372691052", "isim/quantization_test_isim_beh.exe.sim/work/a_3923040884_2372691052.didat");
+	xsi_register_executes(pe);
+}
